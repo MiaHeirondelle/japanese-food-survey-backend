c8f5ed644116479565b592f768023d398e1e6cc2
MiaHeirondelle <miroslava977@gmail.com>
2022-02-04 20:41:10 +0900

    Change session table structure

--- src/main/resources/db/migration/V1__user_session_table.sql
+++ src/main/resources/db/migration/V1__user_session_table.sql
@@ -1,4 +1,4 @@
 CREATE TABLE user_session (
-  user_id    VARCHAR(32) PRIMARY KEY NOT NULL,
-  token_hash VARCHAR(64) UNIQUE NOT NULL
+  token_hash VARCHAR(64) PRIMARY KEY NOT NULL,
+  user_id    VARCHAR(32) NOT NULL
 )

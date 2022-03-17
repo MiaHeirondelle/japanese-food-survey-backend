89d63f34ec1f8c2e054db44566bbe951e8e0f736
Mia <65589274+MiaHeirondelle@users.noreply.github.com>
2022-03-17 18:28:33 +0900

    Create question domain model (#26)

---
+++ src/main/scala/jp/ac/tachibana/food_survey/domain/question/Question.scala
@@ -0,0 +1,28 @@
+package jp.ac.tachibana.food_survey.domain.question
+
+import jp.ac.tachibana.food_survey.domain.question.Question.ScaleText
+
+sealed trait Question:
+  def id: Question.Id
+  def text: String
+  def scaleText: ScaleText
+
+object Question:
+
+  case class ScaleText(
+    minBoundCaption: String,
+    maxBoundCaption: String)
+
+  opaque type Id = String
+
+  case class Basic(
+    id: Question.Id,
+    text: String,
+    scaleText: Question.ScaleText)
+      extends Question
+
+  case class Repeated(
+    id: Question.Id,
+    previousQuestionId: Question.Id,
+    text: String,
+    scaleText: Question.ScaleText)
